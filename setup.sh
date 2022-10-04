eca2397b742f7b01339169d46ca921b915fb607a
Izxyx <arq_h@outlook.com>
2022-10-04 11:35:35 -0500

    Cambio de color

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 [theme]\n\
-primaryColor = '#e7ff0f'\n\
+primaryColor = '#ffe400'\n\
 backgroundColor = '#002b36'\n\
 secondaryBackgroundColor = '#586e75'\n\
 textColor = '#fff'\n\
