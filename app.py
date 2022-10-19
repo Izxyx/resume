04807132cd23b9d9bb3382869aea043d99825c1e
Izxyx <arq_h@outlook.com>
2022-10-18 20:11:30 -0500

    d

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ PROJECTS = {
     '► Example Retail store insights. (Deepnote, Pandas, Seaborn, Plotly, Scipy)': 'https://deepnote.com/@hugo-perez-d799/EDA-Retail-Store-1584efa7-48ca-42f7-a2df-bfe70ebdd983',
     '► Power BI dashboard example.': 'https://app.powerbi.com/groups/me/reports/41b47aa3-eb9e-441a-bc06-6006cf203976/ReportSection',
     '► Excel dashboard example.': 'https://www.youtube.com/watch?v=PyoRdu-i0AQ&ab_channel=DilanxC',
-    '► Google Data Stusio dashboard example.': 'https://www.youtube.com/watch?v=PyoRdu-i0AQ&ab_channel=DilanxC',
+    '► Google Data Studio dashboard example.': 'https://www.youtube.com/watch?v=PyoRdu-i0AQ&ab_channel=DilanxC',
 }
 
 
